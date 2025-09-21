f3f6d2899ec00583e9bd2ea9119497637f931c6e
theorallon <theo_rallon@estudante.sesisenai.org.br>
2025-09-20 21:21:31 -0300

    Adiciona funcionalidade para criar e excluir metas, com melhorias na estrutura HTML e estilo CSS

--- script.js
+++ script.js
@@ -13,11 +13,7 @@ function criarMeta() {
                   document.getElementById("mensagem").textContent = mensagemErro;
             }  else {
             document.getElementById("mensagem").textContent = mensagemConfirmação;
-            }
-
-    
-
-    let body = document.getElementById("body");
+            let body = document.getElementById("body");
 
     let CaixaDeMetas = document.getElementById("CaixaDeMetas");
 
@@ -26,14 +22,47 @@ function criarMeta() {
 
     let tituloMeta = document.createElement("h3");
     tituloMeta.textContent = valorMeta;
-    CaixaDeMetas.appendChild(tituloMeta);
+    meta.appendChild(tituloMeta);
 
+    let icone = document.createElement("ion-icon")
+            //atribuindo o icone lixeira a tag icone
+            icone.setAttribute("name", "trash-bin-outline");
+
+            //conectando ela ao item da lista
+            tituloMeta.appendChild(icone)
+
+            icone.addEventListener('click', excluirItem)
+
+             // função para remover item da lista
+            function excluirItem(){
+            meta.remove()
+                let mensagemExcluirMeta = "Meta excluida"
+                document.getElementById("mensagem").textContent = mensagemExcluirMeta;
+            }
+
+
+            
+
+    //cria a variavel do botão que cria objetivos
     let botãoCriarObjetivo = document.createElement("button");
+    //define o que tem escrito dentro do botão
     botãoCriarObjetivo.textContent = "Criar objetivo";
-    CaixaDeMetas.appendChild(botãoCriarObjetivo);
+    //define que o botão deve ser criado dentro da tag 
+    meta.appendChild(botãoCriarObjetivo);
+
+    //adiciona a função do que acontece quando o botão "botão criar objeto faz"
     botãoCriarObjetivo.addEventListener("click", function() {
         let objetivo = document.createElement("li");
         objetivo.textContent = "Novo objetivo";
         meta.appendChild(objetivo);
     });
+    inputMeta.value = ""
+
+    
+
+            }
+
+    
+
+    
 }
